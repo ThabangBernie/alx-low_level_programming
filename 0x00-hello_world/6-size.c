4d6fcfb45782e982b8cd233357c71b3cec20c383
ThabangBernie <Thabangmotsoatsoa@gmail.com>
2022-10-17 07:24:51 -0700

    My low level size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main -Entry
- * Return: Always 0
+ * main -is the entey point for the program
+ *
+ * Return:zero if no error, Non-zero value if errors.
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of float: %i byte(s)\n", sizeof(float));
+	printf("Size of a char: %id byte(s)\n", sizeof(char));
+	printf("Size of an int: %id byte(s)\n", sizeof(int));
+	printf("Size of a long int: %id byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %id byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %id byte(s)\n", sizeof(float));
 	return (0);
 }
