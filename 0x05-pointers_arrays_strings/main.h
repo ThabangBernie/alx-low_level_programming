e407e0975963bb28bcef174548fa72b73d0bdf95
ThabangBernie <Thabangmotsoatsoa@gmail.com>
2022-10-24 17:06:15 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 void reset_to_98(int *n);
-voi swap_int(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
@@ -12,4 +12,6 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void puts2(char *str);
 int _atoi(char *s);
+int _putchar(char c);
+
 #endif
